6d69a4de30ecc927d4878b311359787ad7932ed4
Renzo Orozco <106353404+RaOS98@users.noreply.github.com>
2025-06-30 17:57:04 -0500

    Updated preprocessing logic, to load and store from S3. Data is now merged, transformed and saved as clean. Modularity fixed for both train and oot data

---
+++ src/preprocessing/config.py
@@ -0,0 +1,6 @@
+RAW_PATHS = {
+    "clientes": "data/raw/train_clientes_sample.csv",
+    "requerimientos": "data/raw/train_requerimientos_sample.csv",
+}
+
+CLEAN_OUTPUT_PATH = "data/processed/clean_data"
